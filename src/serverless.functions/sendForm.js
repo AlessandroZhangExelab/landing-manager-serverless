a643dcc1c1c8ef542afb4f6dd860891604666109
alessandro_zhang <alessandro.zhang@exelab.com>
2024-05-03 17:46:46 +0200

    created project

---
+++ src/serverless.functions/sendForm.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+exports.main = async (context = {}) => {
+  const baseURL = process.env["BASE_URL"];
+  const API_KEY = process.env["API_KEY"];
+  const endpoint = "hubspot/landing/data";
+  const body = context.body;
+
+  try {
+    const config = {
+      method: "POST",
+      url: `${baseURL}${endpoint}`,
+      headers: {
+        "Content-Type": "application/json",
+        API_KEY,
+      },
+      data: body,
+    };
+    const response = await axios(config);
+    return {
+      body: {
+        response: {
+          ok: true,
+        },
+      },
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      body: {
+        message: error.message,
+      },
+      statusCode: 400,
+    };
+  }
+};
